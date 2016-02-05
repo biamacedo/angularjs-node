7836dadd2eb9d991dd44931a60a808ca3be74455
biamacedo <bia.macedo13@gmail.com>
2016-02-05 12:51:21 -0200

    Init Commit

---
+++ js/controllers/myController.js
@@ -0,0 +1,7 @@
+app.controller('myController',['$scope', function($scope){
+    $scope.name= "My First Angular App";
+    $scope.version = "v1.0.1";
+    $scope.author = "Created by <add your name>";
+    $scope.release ="Released on <add a date>";
+    $scope.message ="This app is <add your message>";
+}]);
